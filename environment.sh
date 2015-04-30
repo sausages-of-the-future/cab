d64c99635f11a87b463b11db1c18a27593aecf9d
Richard Pope <richard@memespring.co.uk>
2015-04-30 11:58:06 +0100

    first commit

---
+++ environment.sh
@@ -0,0 +1,8 @@
+export SETTINGS='config.DevelopmentConfig'
+export APP_ROOT='.'
+export SECRET_KEY='local-dev-not-secret'
+export CSRF_ENABLED=True
+export SECURITY_PASSWORD_HASH='bcrypt'
+export BASE_URL='http://cab.gov.local'
+export REGISTRY_BASE_URL='http://registry.gov.local'
+export PAYMENT_BASE_URL='http://payment.gov.local'
